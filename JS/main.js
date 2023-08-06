25ff902114ed6fa968fa4f9a9a9b06ac7ade8bc8
Agustin Defilippi <defilippiagustin@hotmail.com>
2023-08-06 18:39:16 -0300

    proyecto final

--- JS/main.js
+++ JS/main.js
@@ -8,9 +8,5 @@ const datos = JSON.parse(localStorage.getItem('misPedidos')) || []
 let stockProductos = JSON.parse(localStorage.getItem('baseDatos')) || [];
 const pedidosGuardados = JSON.parse(localStorage.getItem('guardarPedidos')) ||[] ;
 
-
-
-
-
 let comprobantePedido =1 
 console.log(comprobantePedido + "alfin");
